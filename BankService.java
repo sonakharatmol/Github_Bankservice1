6bb76af19d1c7e9d53255834a0ddbd8416887971
sonakharatmol <166410931+sonakharatmol@users.noreply.github.com>
2026-04-20 19:16:35 +0530

    Change deposit amount from 5000 to 50

--- BankService.java
+++ BankService.java
@@ -34,7 +34,7 @@ public class BankService {
 
         System.out.println("Initial Balance: " + acc.getBalance());
 
-        acc.deposit(5000);
+        acc.deposit(50);
         System.out.println("Balance after deposit of 500: " + acc.getBalance());
 
         acc.withdraw(40);
